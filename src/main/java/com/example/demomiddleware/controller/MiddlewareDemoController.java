dfb9c48db78e0540ea7cc1567f7e74d746d79feb
Jesus Torres <jtorre58@msudenver.edu>
2019-10-09 21:00:24 -0600

    run on custom port

---
+++ src/main/java/com/example/demomiddleware/controller/MiddlewareDemoController.java
@@ -0,0 +1,16 @@
+package com.example.demomiddleware.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MiddlewareDemoController {
+
+    @RequestMapping(value = "/getDateAndTime")
+    public ResponseEntity<String> getTimeDate(){
+        ResponseEntity<String> response;
+        return new ResponseEntity<>("Null Value", HttpStatus.OK);
+    }
+}
