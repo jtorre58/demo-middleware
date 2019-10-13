150e43e13cd670098bf749ae348de5e357e4bd99
Jesus Torres <jtorre58@msudenver.edu>
2019-10-12 23:00:03 -0600

    Testing travis ci build part 2

--- src/main/java/com/example/demomiddleware/controller/MiddlewareDemoController.java
+++ src/main/java/com/example/demomiddleware/controller/MiddlewareDemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MiddlewareDemoController {
-    //Testing build
+    //Testing Travis ci build
     @RequestMapping(value = "/getDateAndTime")
     public ResponseEntity<String> getTimeDate(){
         ResponseEntity<String> response;
