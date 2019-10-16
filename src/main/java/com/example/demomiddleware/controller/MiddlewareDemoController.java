a4bb1b1bc25a73f7d3720eace6b78fdf91973fe2
Jesus Torres <jtorre58@msudenver.edu>
2019-10-15 21:55:48 -0600

    Added swagger functionality

--- src/main/java/com/example/demomiddleware/controller/MiddlewareDemoController.java
+++ src/main/java/com/example/demomiddleware/controller/MiddlewareDemoController.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MiddlewareDemoController {
     //Testing Travis ci build
-    @RequestMapping(value = "/getDateAndTime")
+    @RequestMapping(value = "/getDateAndTime", method = RequestMethod.GET)
     public ResponseEntity<String> getTimeDate(){
         ResponseEntity<String> response;
         return new ResponseEntity<>("Null Value", HttpStatus.OK);
